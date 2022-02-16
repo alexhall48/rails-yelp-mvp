1cc2b8d3eb53456f3ce2f6cd4ee4c680b3938cb6
alex hall <alex_hall@hotmail.co.uk>
2022-02-16 14:40:44 +0000

    seeds

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+  CATEGORY = %w(chinese italian japanese french belgian)
+
   validates :name, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
